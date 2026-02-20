1588a44a20696ab184bed50fe04997220b5af84a
Baptiste JULLION <contact@baptiste-jullion.dev>
2026-02-20 11:56:43 +0100

    feat: add subscription and unsubscription endpoints for topics in TopicController and implement corresponding service methods

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -7,21 +7,39 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/topics")
 @RequiredArgsConstructor
 @Tag(name = "Topic")
 public class TopicController {
     private final TopicService topicService;
 
     @Operation(summary = "List all topics")
-    @GetMapping("/api/topics")
+    @GetMapping
     public ResponseEntity<List<TopicResponse>> list() {
         return ResponseEntity.status(HttpStatus.OK)
                              .body(topicService.getAllTopics());
+    }
+
+    @Operation(summary = "Subscribe to a topic")
+    @PostMapping("/{topicId}/subscribe")
+    public ResponseEntity<Void> subscribe(@PathVariable UUID topicId, Principal principal) {
+        topicService.subscribe(topicId, principal.getName());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                             .build();
+    }
+
+    @Operation(summary = "Unsubscribe from a topic")
+    @DeleteMapping("/{topicId}/unsubscribe")
+    public ResponseEntity<Void> unsubscribe(@PathVariable UUID topicId, Principal principal) {
+        topicService.unsubscribe(topicId, principal.getName());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                             .build();
     }
 }
